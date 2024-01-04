bdf9b695e189f0660faf15ce50aa7f1d8b30343b
Hanumat Sharan <deotadayal27@gmail.com>
2024-01-04 13:28:13 +0530

    Update HomePage.jsx

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -40,38 +40,38 @@ const HomePage = () => {
       <CartItems/>
       <Box className="black_bcg">
         <img
-          src="https://cdn.plotch.io/image/upload/C/V/1660916598_MS5wbmc=.png"
+          src="https://s.rmjo.in/Price-drop-hp-web.jpg"
           onClick={() => navigate("/products")}
         />
         <img
-          src="https://cdn.plotch.io/image/upload/C/V/1660916607_Mi5wbmc=.png"
+          src="https://s.rmjo.in/Price-drop-hp-web.jpg"
           onClick={() => navigate("/products")}
         />
         <img
-          src="https://cdn.plotch.io/image/upload/C/V/1660916612_My5wbmc=.png"
+          src="https://s.rmjo.in/Price-drop-hp-web.jpg"
           onClick={() => navigate("/products")}
         />
         <img
-          src="https://cdn.plotch.io/image/upload/C/V/1660916617_NC5wbmc=.png"
+          src="https://s.rmjo.in/Price-drop-hp-web.jpg"
           onClick={() => navigate("/products")}
         />
         <img
-          src="https://cdn.plotch.io/image/upload/C/V/1660916629_NS5wbmc=.png"
+          src="https://s.rmjo.in/Price-drop-hp-web.jpg"
           onClick={() => navigate("/products")}
         />
       </Box>
       <Box className="four_large_img">
-        <img src="https://cdn.plotch.io/image/upload/C/V/1671110234_SG9tZWRlY29yLnBuZw==.png" />
+        <img src="https://s.rmjo.in/Price-drop-hp-web.jpg" />
         <img
-          src="https://cdn.plotch.io/image/upload/C/V/1671110241_NS5wbmc=.png"
+          src="https://s.rmjo.in/Price-drop-hp-web.jpg"
           onClick={() => navigate("/products")}
         />
         <img
-          src="https://cdn.plotch.io/image/upload/C/V/1671110248_Ni5wbmc=.png"
+          src="https://s.rmjo.in/Price-drop-hp-web.jpg"
           onClick={() => navigate("/products")}
         />
         <img
-          src="https://cdn.plotch.io/image/upload/C/V/1671110264_My5wbmc=.png"
+          src="https://s.rmjo.in/Price-drop-hp-web.jpg"
           onClick={() => navigate("/products")}
         />
       </Box>
